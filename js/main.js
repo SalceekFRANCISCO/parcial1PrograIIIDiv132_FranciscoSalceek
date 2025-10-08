f2d326f0503d2ce783b98db58b8278ddf6c764e6
franciscosalceek1 <salceekmunozfrancisco@gmail.com>
2025-10-08 11:06:54 -0300

    actualizado

--- js/main.js
+++ js/main.js
@@ -1,79 +1,51 @@
-// variables
-let contenedorLibros = document.querySelector("#seccionlibros");
+//#region variables
+let contenedorFrutas = document.querySelector("#seccionFrutas");
 
 let botonBuscar = document.querySelector("#barraBusqueda");
 
 let carrito = document.querySelector("#asidecarrito");
 
+let carritocantidad = document.querySelector("#cantidad-carrito");//
+
 let botonMenorAMayor = document.querySelector("#ordenarMenorMayor");
 
-let botonMayorAMenor = document.querySelector("#ordenarMayorMenor");
-
 let ordenarNombre = document.querySelector("#ordenarNombre");
+//#endregion
 
-//array de botones
-const libros = [
-    {id: 1, nombre: 'Cien años de soledad', precio: 8500, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 1, titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez', precio: 8500, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 2, titulo: 'El principito', autor: 'Antoine de Saint-Exupéry', precio: 5200, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 3, titulo: '1984', autor: 'George Orwell', precio: 6900, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 4, titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', precio: 9700, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 5, titulo: 'Fahrenheit 451', autor: 'Ray Bradbury', precio: 6400, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 6, titulo: 'Orgullo y prejuicio', autor: 'Jane Austen', precio: 7100, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 7, titulo: 'Crimen y castigo', autor: 'Fiódor Dostoyevski', precio: 9300, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 8, titulo: 'El señor de los anillos', autor: 'J. R. R. Tolkien', precio: 11800, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 9, titulo: 'La sombra del viento', autor: 'Carlos Ruiz Zafón', precio: 7800, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 10, titulo: 'Matar a un ruiseñor', autor: 'Harper Lee', precio: 8600, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 11, titulo: 'Los miserables', autor: 'Victor Hugo', precio: 9900, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-    {id: 12, titulo: 'El alquimista', autor: 'Paulo Coelho', precio: 6200, ruta_img: 'img/cien_anos_de_soledad.jpg'},
+//#region array de frutas
+   const frutas = [
+  { id: 1, nombre: "arandano", precio: 5000, ruta_img: "img/arandano.jpg" },
+  { id: 2, nombre: "banana", precio: 1000, ruta_img: "img/banana.jpg" },
+  { id: 3, nombre: "frambuesa", precio: 4000, ruta_img: "img/frambuesa.png" },
+  { id: 4, nombre: "frutilla", precio: 3000, ruta_img: "img/frutilla.jpg" },
+  { id: 5, nombre: "kiwi", precio: 2000, ruta_img: "img/kiwi.jpg" },
+  { id: 6, nombre: "mandarina", precio: 800, ruta_img: "img/mandarina.jpg" },
+  { id: 7, nombre: "manzana", precio: 1500, ruta_img: "img/manzana.jpg" },
+  { id: 8, nombre: "naranja", precio: 9000, ruta_img: "img/naranja.jpg" },
+  { id: 9, nombre: "pera", precio: 2500, ruta_img: "img/pera.jpg" },
+  { id: 10, nombre: "anana", precio: 5000, ruta_img: "img/anana.jpg" },
+  { id: 11, nombre: "pomelo-amarillo", precio: 2000, ruta_img: "img/pomelo-amarillo.jpg" },
+  { id: 12, nombre: "pomelo-rojo", precio: 5000, ruta_img: "img/pomelo-rojo.jpg" },
 ];
-
-//#region libros con url corregida
-// const libros = [
-//     {id: 1, titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez', precio: 8500, ruta_img: 'img/cien_anos_de_soledad.jpg'},
-//     {id: 2, titulo: 'El principito', autor: 'Antoine de Saint-Exupéry', precio: 5200, ruta_img: 'img/el_principito.jpg'},
-//     {id: 3, titulo: '1984', autor: 'George Orwell', precio: 6900, ruta_img: 'img/1984.jpg'},
-//     {id: 4, titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', precio: 9700, ruta_img: 'img/don_quijote.jpg'},
-//     {id: 5, titulo: 'Fahrenheit 451', autor: 'Ray Bradbury', precio: 6400, ruta_img: 'img/fahrenheit_451.jpg'},
-//     {id: 6, titulo: 'Orgullo y prejuicio', autor: 'Jane Austen', precio: 7100, ruta_img: 'img/orgullo_y_prejuicio.jpg'},
-//     {id: 7, titulo: 'Crimen y castigo', autor: 'Fiódor Dostoyevski', precio: 9300, ruta_img: 'img/crimen_y_castigo.jpg'},
-//     {id: 8, titulo: 'El señor de los anillos', autor: 'J. R. R. Tolkien', precio: 11800, ruta_img: 'img/el_senor_de_los_anillos.jpg'},
-//     {id: 9, titulo: 'La sombra del viento', autor: 'Carlos Ruiz Zafón', precio: 7800, ruta_img: 'img/la_sombra_del_viento.jpg'},
-//     {id: 10, titulo: 'Matar a un ruiseñor', autor: 'Harper Lee', precio: 8600, ruta_img: 'img/matar_a_un_ruisenor.jpg'},
-//     {id: 11, titulo: 'Los miserables', autor: 'Victor Hugo', precio: 9900, ruta_img: 'img/los_miserables.jpg'},
-//     {id: 12, titulo: 'El alquimista', autor: 'Paulo Coelho', precio: 6200, ruta_img: 'img/el_alquimista.jpg'},
-//     {id: 13, titulo: 'It', autor: 'Stephen King', precio: 10500, ruta_img: 'img/it.jpg'},
-//     {id: 14, titulo: 'El nombre del viento', autor: 'Patrick Rothfuss', precio: 9400, ruta_img: 'img/el_nombre_del_viento.jpg'},
-//     {id: 15, titulo: 'Drácula', autor: 'Bram Stoker', precio: 8100, ruta_img: 'img/dracula.jpg'},
-//     {id: 16, titulo: 'La metamorfosis', autor: 'Franz Kafka', precio: 5600, ruta_img: 'img/la_metamorfosis.jpg'},
-//     {id: 17, titulo: 'Los juegos del hambre', autor: 'Suzanne Collins', precio: 7200, ruta_img: 'img/los_juegos_del_hambre.jpg'},
-//     {id: 18, titulo: 'Harry Potter y la piedra filosofal', autor: 'J. K. Rowling', precio: 8900, ruta_img: 'img/harry_potter_piedra_filosofal.jpg'}
-// ];
 //#endregion
 
 
 let favoritos = [];
-let listadoLibros= "";
+let listadoFrutas= "";
 
 function ordenar(){
     botonMenorAMayor.addEventListener("click", function(){
-        libros.sort((lib1,lib2)=> lib1.precio - lib2.precio);
-        mostrarListaLibros(libros);
+        frutas.sort((fruta1,lib2)=> fruta1.precio - lib2.precio);
+        mostrarListaFrutas(frutas);
     });
 }
 
-function ordenar2(){
-    botonMayorAMenor.addEventListener("click", function(){
-        libros.sort((lib1,lib2)=> lib2.precio - lib1.precio);
-        mostrarListaLibros(libros);
-    });
-}
-
-function ordenar3(){
+// ordenamiento por nombre (toem la idea de la docuemntacion de sort en w3schools)
+function ordenarPorNombre(){
     ordenarNombre.addEventListener("click", function(){
-        libros.sort(function(lib1,lib2){
-            let x = lib1.titulo.toUpperCase();
-            let y = lib2.titulo.toUpperCase();
+        frutas.sort(function(fruta1,fruta2){
+            let x = fruta1.nombre.toUpperCase();
+            let y = fruta2.nombre.toUpperCase();
             let resultado=0;
             if (x < y){
                 resultado = -1;
@@ -83,10 +55,11 @@ function ordenar3(){
             } 
             return resultado;
         });
-        mostrarListaLibros(libros);
+        mostrarListaFrutas(frutas);
     });
 }
 
+// punto 5 mostrar carrito
 function mostrarCarrito(){
     let cantidadCarrito = favoritos.length;
 
@@ -95,17 +68,21 @@ function mostrarCarrito(){
     }
     else{
         let total = calcularTotal(favoritos);
+        let cantidadProductosCarrito = `<p> Carrito:${cantidadCarrito} Productos</p>`;
         let carritoCargado = "<ul class=listaCarrito> ";
-        favoritos.forEach((libfav,indice) => {
+        carritoCargado +=`<h2 class="carrito-titulo">Carrito</h2>`
+        favoritos.forEach((fruta,indice) => {
                 carritoCargado +=
-                `<li>
-                    <p>${libfav.titulo}-- $${libfav.precio}</p>
+                `<li class="bloque-item">
+                    <p class="nombre-item">${fruta.nombre}-- $${fruta.precio}</p>
                     <button class="botonEliminar" onclick="eliminarLibro(${(indice)})">Eliminar</button>
                 </li>`
         });
 
         carritoCargado += `<h3>Total de carrito: $ ${total}</p> </h3>`;
         carritoCargado += `<p> Productos:  ${cantidadCarrito} </p>`;
+        carritoCargado += `<button class="vaciarCarrito" onclick="vaciarCarrito(${(indice)})">Eliminar</button>`;
+        // carritocantidad.innerHTML += cantidadProductosCarrito;
         carrito.innerHTML = carritoCargado;
     }
     
@@ -134,32 +111,32 @@ function calcularTotal(array) {
     return total;
 }
 
-function mostrarListaLibros(array){
-    let listadoLibros= "<ul class=lista> ";
-    array.forEach(libro => {
-        listadoLibros += 
-        `<li id=listaLibros>
-            <img class=imgLibro src=${libro.ruta_img} alt="${libro.titulo}">
-            <h2 class=autor>${libro.titulo}</h2>
-            <h5 class=autor> ${libro.autor}</h5>
-            <p>$${libro.precio}</p>
-            <button class="botonAgregar" onclick="agregarLibro(${libro.id})">Agregar a favoritos</button>
+function mostrarListaFrutas(array){
+    let listadoFrutas= "<ul class=lista> ";
+    array.forEach(fruta => {
+        listadoFrutas += 
+        `<li id=listaFrutas>
+            <img class=imgLibro src=${fruta.ruta_img} alt="${fruta.nombre}">
+            <h2 class=autor>${fruta.nombre}</h2>
+            <p>$${fruta.precio}</p>
+            <button class="botonAgregar" onclick="agregarLibro(${fruta.id})">Agregar a favoritos</button>
         </li>`;
     });
-    listadoLibros += "</ul>";
-    contenedorLibros.innerHTML = listadoLibros;
+    listadoFrutas += "</ul>";
+    contenedorFrutas.innerHTML = listadoFrutas;
     
 }
 
+// para buscar frutas el punto 4
 function buscar(){
     botonBuscar.addEventListener("keydown",function(){
         
         let valorBusqueda = botonBuscar.value;
 
-        let librosFiltrados = libros.filter(lib => lib.titulo.includes(valorBusqueda));
-        console.log(librosFiltrados);
+        let frutasFiltrados = frutas.filter(fruta => fruta.nombre.includes(valorBusqueda));
+        console.log(frutasFiltrados);
 
-        mostrarListaLibros(librosFiltrados);
+        mostrarListaFrutas(frutasFiltrados);
         
     });
 }
@@ -179,17 +156,16 @@ function validarExistencia(libro){
 }
 
 function agregarLibro(id){
-    let libro = libros.find(libro => libro.id == id);
+    let libro = frutas.find(libro => libro.id == id);
     validarExistencia(libro);
     mostrarCarrito();
 }
 
 function init(){
-    mostrarListaLibros(libros); 
+    mostrarListaFrutas(frutas); 
     buscar();
     ordenar();
-    ordenar2();
-    ordenar3();
+    ordenarPorNombre();
 }
 
 init();
